2c59b5b96567c5866a8b7f3feb485f0cdf2c3ed0
Jefren Aguilar <jefren.aguilar@value-exch.com>
2023-03-13 11:44:37 +0800

    Initial commit

---
+++ src/test/java/org/tap/enrollment/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.tap.enrollment;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
