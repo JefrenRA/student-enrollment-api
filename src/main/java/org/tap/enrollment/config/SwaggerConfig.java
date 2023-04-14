121eb07e0a3c915f90fbea57f60bef618d546d51
Jefren Aguilar <jefren.aguilar@value-exch.com>
2023-04-14 12:19:48 +0800

    Created AccountUserDetailsService to handle Basic authentication in WebSecurityConfig

--- src/main/java/org/tap/enrollment/config/SwaggerConfig.java
+++ src/main/java/org/tap/enrollment/config/SwaggerConfig.java
@@ -1,20 +1,13 @@
 package org.tap.enrollment.config;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.google.common.net.HttpHeaders;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.BasicAuth;
 import springfox.documentation.service.SecurityReference;
@@ -26,35 +19,35 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig implements WebMvcConfigurer{
+public class SwaggerConfig implements WebMvcConfigurer {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(basicAuthScheme()));
-   }
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.securityContexts(Arrays.asList(securityContext()))
+				.securitySchemes(Arrays.asList(basicAuthScheme()))
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(Arrays.asList(basicAuthReference()))
-                .forPaths(PathSelectors.ant("/.*"))
-                .build();
-    }
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(Arrays.asList(basicAuthReference()))
+				.forPaths(PathSelectors.ant("/.*"))
+				.build();
+	}
 
-    private SecurityScheme basicAuthScheme() {
-        return new BasicAuth("basicAuth");
-    }
+	private SecurityScheme basicAuthScheme() {
+		return new BasicAuth("basicAuth");
+	}
 
-    private SecurityReference basicAuthReference() {
-    	AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
-    	AuthorizationScope[] scopes = new AuthorizationScope[1];
-    	scopes[0] = scope;
-        return new SecurityReference("basicAuth", scopes);
-    }
+	private SecurityReference basicAuthReference() {
+		AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] scopes = new AuthorizationScope[1];
+		scopes[0] = scope;
+		return new SecurityReference("basicAuth", scopes);
+	}
 
 }
--- src/main/java/org/tap/enrollment/config/WebSecurityConfig.java
+++ src/main/java/org/tap/enrollment/config/WebSecurityConfig.java
@@ -1,70 +1,79 @@
 package org.tap.enrollment.config;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.tap.enrollment.entity.account.Account;
-import org.tap.enrollment.repository.account.AccountRepository;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private static final String[] SWAGGER = { "/v2/api-docs", "/swagger-resources/", "/swagger-ui.html", "/webjars/", "favicon.ico", "/error/" };
+	private static final String[] SWAGGER = { "/v2/api-docs", "/swagger-resources/", "/swagger-ui.html", "/webjars/",
+			"favicon.ico", "/error/" };
+
+//	@Autowired
+//	private AccountRepository accRepository;
 	
 	@Autowired
-	private AccountRepository accRepository;
-
+	private UserDetailsService userDetailsService;
 
 	@Override
-    public void configure(HttpSecurity http) throws Exception {
-    	http.csrf().disable()
-    		.headers().frameOptions().disable()
-            .and()
-    		.authorizeRequests() 
-    		.antMatchers("/h2/**").permitAll()
-	    	.antMatchers(SWAGGER).permitAll()
-	        .antMatchers("/enrollment/**").hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
-	        .antMatchers("/course-enrollment/**").hasAuthority("ROLE_ADMIN")
-	        .antMatchers("/subject-enrollment/**").hasAuthority("ROLE_ADMIN")
-	        .antMatchers("/account/**").hasAuthority("ROLE_ADMIN")
-	        .anyRequest().authenticated();
-	        
-    }
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-		
-		long totalRecord = accRepository.count();
-		long id = 1;
-		while(id<=totalRecord){
-			Optional <Account> user = this.accRepository.findById(id);
-	        if(user.isPresent()) {
-	        	Account acc = user.get();
-	        	
-	        	auth.inMemoryAuthentication()
-					.withUser(acc.getUsername())
-					.password(passwordEncoder().encode(acc.getPassword()))
-					.authorities("ROLE_"+ acc.getRole());
-	        }
-	        id++;
-		}
+	public void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+				.authorizeRequests()
+				.antMatchers("/h2/**").permitAll()
+				.antMatchers(SWAGGER).permitAll()
+				.antMatchers("/course-enrollment/**").hasRole("ADMIN")
+				.antMatchers("/subject-enrollment/**").hasRole("ADMIN")
+				.antMatchers("/account/**").hasRole("ADMIN")
+				.anyRequest().authenticated();
+
 	}
-	
+
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//		long totalRecord = accRepository.count();
+//		long id = 1;
+//		while (id <= totalRecord) {
+//			Optional<Account> user = this.accRepository.findById(id);
+//			if (user.isPresent()) {
+//				Account acc = user.get();
+//
+//				auth.inMemoryAuthentication().withUser(acc.getUsername())
+//						.password(passwordEncoder().encode(acc.getPassword())).authorities("ROLE_" + acc.getRole());
+//			}
+//			id++;
+//		}
+//	}
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
 }
